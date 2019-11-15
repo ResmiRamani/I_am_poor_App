d54f71d4574e2b61b00002d9055e5ae4c69071ca
ResmiRamani <Resmi.RamaniAmma@triosstudent.com>
2019-11-15 11:07:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/i_am_poor_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.i_am_poor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
